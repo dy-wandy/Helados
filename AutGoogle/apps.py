0bd581c6d617b2c99004e08b57495991932640c0
Wandy Olivares <dy.wandy@icloud.com>
2023-10-29 12:16:12 -0400

    Feact(Diseño principal terminado)

---
+++ AutGoogle/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutgoogleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AutGoogle'
--- Helados/settings.py
+++ Helados/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
+    
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps_Code_Olivares
+    'AutGoogle.apps.AutgoogleConfig',
+    'app.apps.AppConfig',
+
 ]
 
 MIDDLEWARE = [
